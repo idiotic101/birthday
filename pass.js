5c5f32cf324054546c91ed09837a95a261d5d7f1
karnik101 <166645166+karnik101@users.noreply.github.com>
2024-06-14 10:06:24 +0530

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,12 @@
+var password="7";
+function passcheck() {
+
+if(document.getElementById('pass1').value != password){
+alert('you are just dumb bro , and if aarush u are trying stop it get some help');
+return false;
+}
+if(document.getElementById('pass1').value == password){
+alert('thankyou, you may pass');
+}
+}
+
